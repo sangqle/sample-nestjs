d9fa7e37869c8fc2fbeb01ecaba91859eaa7a11e
sanglq3 <sanglq3@vng.com.vn>
2021-11-19 17:32:20 +0700

    remove mongo uri

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,9 +3,6 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        'mongodb+srv://littleguppies:sanglq3@cluster0.gtmb6.mongodb.net/quotes',
-      ),
+    useFactory: (): Promise<typeof mongoose> => mongoose.connect(''),
   },
 ];
